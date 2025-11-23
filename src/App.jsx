9ec91d38973e9a4d1ca398b9719073c4d6ed5991
Nasser Hussein <nasser@index.local>
2025-11-23 10:48:26 +0200

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,20 @@
-import { useState } from 'react'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
-// import DeepZoomViewer from './Components/DeepZoom/DeepZoomViewer';
-import OverlayMarker2 from './Components/OverlayMarker2/OverlayMarker2';
-import OverlayMarker from './Components/OpenSeadragon/OverlayMarker';
-import DeepZoomViewer from './Components/OpenSeadragon/DeepZoomViewer';
+import Layout from './Layout'
+import V1 from './Components/V1/V1'
+import V2 from './Components/V2/V2'
+
 
 function App() {
-  const marks = [
-        {
-            "id": 1,
-            "name": "Cairo",
-            "iso_code": "EG",
-            "coordinate_x": 5.43,
-            "coordinate_y": -17 ,
-            "country_map_img": "storage/assets/images/countries/maps/deepzoomfolders/690bc465814e31762378853/output_tiles5.dzi",
-            "navigate_to": "cairo/october-map",
-            "targetZoom": 15,
-            "flag_icon": "assets/EgyptMap/media/Cairo.svg",
-            "flag": "assets/tiles7/EgyptFlage.svg",
-            "world_id": 1,
-            "created_at": "2025-11-05T21:40:55.000000Z",
-            "updated_at": "2025-11-16T09:45:05.000000Z",
-            "zone": {
-                "id": 1,
-                "zone_img": "assets/OctoberMap/CairoWest.svg",
-                "zone_img_hover": "assets/OctoberMap/CairoWestHover.svg",
-                "name": "Cairo",
-                "coordinate_x": 0.478125,
-                "coordinate_y": 0.2759095833333334,
-                "scale": 1,
-                "country_id": 1,
-                "world_id": 1,
-                "created_at": "2025-11-05T21:41:52.000000Z",
-                "updated_at": "2025-11-10T18:04:20.000000Z"
-            }
-        },
-        {
-            "id": 2,
-            "name": "Hurghada",
-            "iso_code": "UK",
-            "coordinate_x": -9.3,
-            "coordinate_y": -19.1,
-            "country_map_img": "storage/assets/images/countries/maps/deepzoomfolders/691228a4ae1011762797732/Rectangle.dzi",
-            "flag_icon": "assets/EgyptMap/media/Hurghada.svg",
-            "flag": "assets/tiles7/UkFlage.svg",
-            "world_id": 1,
-            "created_at": "2025-11-10T18:02:12.000000Z",
-            "updated_at": "2025-11-10T18:02:12.000000Z",
-            "zone": {
-                "id": 2,
-                "zone_img": "assets/EgyptMap/media/HurghadaZone.svg",
-                "zone_img_hover": "assets/EgyptMap/media/HurghadaZoneHover.svg",
-                "name": "Hurghada",
-                "coordinate_x": 0.6604829373666086,
-                "coordinate_y": 0.3547061815179243,
-                "scale": 1,
-                "country_id": 2,
-                "world_id": 1,
-                "created_at": "2025-11-10T18:08:51.000000Z",
-                "updated_at": "2025-11-10T18:08:51.000000Z"
-            }
-        }
-    ];    
+  const router = createBrowserRouter([
+    {path: '', element: <Layout/>, children: [
+        {index: true, element: <V1/>},
+        {path: 'v2', element: <V2/>}
+    ]},
+  ])
   return <>
-  <DeepZoomViewer dziSource="./assets/OctoberMap/OctoberMap.dzi" maxZoomLevel={25} marks={marks} OverlayMarker={OverlayMarker} /> 
+  <RouterProvider router={router}/>
   </>
 }
 
--- src/Components/OpenSeadragon/viewer.js
+++ src/Components/OpenSeadragon/viewer.js
@@ -2,99 +2,107 @@
 import OpenSeadragon from "openseadragon";
 
 export function initViewer(container, dziSource, maxZoomLevel = 5) {
-// return OpenSeadragon({
-//   element: container,
-//       prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
-//       tileSources: dziSource,
-//       showNavigator: false,
-//       animationTime: 1.75,
-//       blendTime: 0.15,
-//       zoomPerScroll: 1.2,
-//       minZoomLevel: 1,
-//       maxZoomLevel: maxZoomLevel || 5,
-//       //
-//       timeout: 100,
-//       preserveImageSizeOnResize: false,
-//       imageLoaderLimit: 8,
-//       maxZoomPixelRatio: 2,
-//       // tileCacheSize: 500,
-//       maxImageCacheCount: 1000,
-//       loadTilesWithAjax: false,
-//       constrainDuringPan: true,
-//       debugMode: false,
-//       defaultZoomLevel: 1,
-//       navigationControlAnchor: OpenSeadragon.ControlAnchor.TOP_RIGHT,
-//       homeFillsViewer: true,
-//       immediateRender: false,
-//       autoResize: true,
-//       background: 'black',
-//       visibilityRatio: 1,
-//       minZoomImageRatio: 0.7,
-//       drawerOptions: {
-//         webgl: true,
-//         canvas: false,
-//       },
-//       smoothTileEdgesMinZoom: 1.25,
-//       gestureSettingsMouse: {
-//         clickToZoom: false,
-//         dblClickToZoom: true,
-//       },
-//       showNavigationControl: false,
-//       showZoomControl: false,
-//       showHomeControl: false,
-//       showFullPageControl: false,
-//       showRotationControl: false,
-//       showFlipControl: false,
-//       showSequenceControl: false,
-//       rotateRightButton: null,
-//       rotateLeftButton: null,
-//     });
-  return OpenSeadragon({
-    element: container,
-    prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
-    tileSources: dziSource,
+return OpenSeadragon({
+  element: container,
+      prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
+      tileSources: dziSource,
+      showNavigator: false,
+      animationTime: 0.75,
+      blendTime: 0.15,
+      zoomPerScroll: 1.2,
+      minZoomLevel: 1,
+      maxZoomLevel: maxZoomLevel || 5,
+      //
+      timeout: 5000,
+      preserveImageSizeOnResize: false,
+      imageLoaderLimit: 8,
+      maxZoomPixelRatio: 2,
+      // tileCacheSize: 500,
+      maxImageCacheCount: 200,
+      loadTilesWithAjax: false,
+      constrainDuringPan: true,
+      debugMode: false,
+      defaultZoomLevel: 1,
+      navigationControlAnchor: OpenSeadragon.ControlAnchor.TOP_RIGHT,
+      homeFillsViewer: true,
+      immediateRender: false,
+      autoResize: true,
+      background: 'black',
+      visibilityRatio: 1,
+      minZoomImageRatio: 0.7,
+      drawerOptions: {
+        webgl: true,
+        canvas: false,
+      },
+      // mobile
+      imageSmoothingEnabled: true,
+      springStiffness: 60,
+      smoothTileEdgesMinZoom: 1.25,
+      gestureSettingsTouch: {
+        pinchRotate: false,
+        flickEnabled: true,
+      },
+      
+      gestureSettingsMouse: {
+        clickToZoom: false,
+        dblClickToZoom: true,
+      },
+      showNavigationControl: false,
+      showZoomControl: false,
+      showHomeControl: false,
+      showFullPageControl: false,
+      showRotationControl: false,
+      showFlipControl: false,
+      showSequenceControl: false,
+      rotateRightButton: null,
+      rotateLeftButton: null,
+    });
+  // return OpenSeadragon({
+  //   element: container,
+  //   prefixUrl: "https://openseadragon.github.io/openseadragon/images/",
+  //   tileSources: dziSource,
 
-    // Performance
-    animationTime: 1.75,
-    blendTime: 0.15,
-    loadTilesWithAjax: false,
-    imageLoaderLimit: 6,
-    tileCacheSize: 60,
-    maxImageCacheCount: 60,
-    maxZoomPixelRatio: 2,
+  //   // Performance
+  //   animationTime: 1.75,
+  //   blendTime: 0.15,
+  //   loadTilesWithAjax: false,
+  //   imageLoaderLimit: 6,
+  //   tileCacheSize: 60,
+  //   maxImageCacheCount: 60,
+  //   maxZoomPixelRatio: 2,
 
-    // Zoom & Pan
-    zoomPerScroll: 1.2,
-    minZoomLevel: 1,
-    maxZoomLevel,
-    homeFillsViewer: true,
-    preserveImageSizeOnResize: true,
+  //   // Zoom & Pan
+  //   zoomPerScroll: 1.2,
+  //   minZoomLevel: 1,
+  //   maxZoomLevel,
+  //   homeFillsViewer: true,
+  //   preserveImageSizeOnResize: true,
     
-    // Animation settings
-    springStiffness: 6.5,
-    immediateRender: true,
+  //   // Animation settings
+  //   springStiffness: 6.5,
+  //   immediateRender: true,
 
-    // UI
-    showNavigator: false,
-    showNavigationControl: true,
+  //   // UI
+  //   showNavigator: false,
+  //   showNavigationControl: true,
 
-    // Canvas better for iPad
-    drawerOptions: {
-      webgl: false,
-      canvas: true,
-    },
-    // Additional settings
-    constrainDuringPan: true,
-    debugMode: false,
-    defaultZoomLevel: 1,
-    autoResize: true,
-    visibilityRatio: 1,
-    smoothTileEdgesMinZoom: 0,
-    gestureSettingsMouse: {
-      clickToZoom: false,
-      dblClickToZoom: true,
-    },
+  //   // Canvas better for iPad
+  //   drawerOptions: {
+  //     webgl: false,
+  //     canvas: true,
+  //   },
+  //   // Additional settings
+  //   constrainDuringPan: true,
+  //   debugMode: false,
+  //   defaultZoomLevel: 1,
+  //   autoResize: true,
+  //   visibilityRatio: 1,
+  //   smoothTileEdgesMinZoom: 0,
+  //   gestureSettingsMouse: {
+  //     clickToZoom: false,
+  //     dblClickToZoom: true,
+  //   },
 
-    background: "black",
-  });
+  //   background: "black",
+  // });
 }
